0584dbb0878585a7985dac77e5ba57db0f495038
MaijaHonig <mh3155@nyu.edu>
2018-02-25 18:35:56 -0500

    packages

--- convertjson_mh.R
+++ convertjson_mh.R
@@ -1,22 +1,37 @@
-
-#convert nyzd to geojson
+#### Converting Shapefiles to GEOJSON
 #https://blog.exploratory.io/creating-geojson-out-of-shapefile-in-r-40bc0005857d
 
-# INPUTS: 
-# your path 
-p = "/Users/Maija/Documents/Python/waterfrontmap/Data/"
-# folder name / layer name 
-n = "nyzd"
+#### Import/load library
 
+install.packages('rgdal')
+install.packages('spdplyr')
+install.packages('geojsonio')
+install.packages('geojsonlint')
+install.packages('rmapshaper')
+
+library(rgdal)
+library(spdplyr)
+library(geojsonio)
+library(geojsonlint)
+library(rmapshaper)
+
+#####################
+# INPUTS: 
+# folder name / layer name 
+name = "nyzd"
+# your path to data folder 
+mypath = "/Users/Maija/Documents/Python/waterfrontmap/Data/"
+#####################
+setwd(mypath)
 
 # NOW WE CONVERT! 
 #Read the data
-censustract <- readOGR(dsn = paste0(p,n),
-layer = n, 
+censustract <- readOGR(dsn = paste0(mypath,name),
+layer = name, 
 verbose = FALSE)
 #Simplify the geometry information of GeoJSON.
 censustractsim <- ms_simplify(censustract)
 #Now convert to geojson
 censustract_json <- geojson_json(censustractsim)
 #Finally, write file
-geojson_write(censustract_json, file = paste0(p,n,"_converted.geojson"))
+geojson_write(censustract_json, file = paste0(mypath,name,"_converted.geojson"))
