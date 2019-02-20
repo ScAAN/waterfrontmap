0481d7c0d7bf69bfaca019528f76b0d8300200f2
maijahonig <mh3155@nyu.edu>
2019-02-20 15:01:50 -0500

    cleaning up

--- Data/Pulling_In_Zoning_GeoJson_Layer.R
+++
@@ -1,9 +0,0 @@
-test <- readLines(url("http://data.beta.nyc//dataset/635e26b3-2acf-4f55-8780-2619660fdf66/resource/e5528464-9a00-40a7-8b85-21e9b25d6c24/download/d52d598c77484806876b8f897d51f805nyczoning.geojson"))
-
-library(geojson)
-
-exp <- as.geojson(test)
-
-exp <- geojson_json(test)
-
-geojson_write(exp, file = paste0("/Users/juliansaliani/Downloads/nyzdtest4.geojson"))
